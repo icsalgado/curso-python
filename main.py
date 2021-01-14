edd031748123d4809a955145fe65a3f71f5f81e8
icsalgado <icsalgado@icloud.com>
2021-01-14 13:46:13 -0300

    Metodo construtor e heranças

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-class circulos:
-    raio = 25.8
+"""
+Método construtor é invocado quando a classe é instanciada, que é o nome dado
+à ação de criar objeros a partir de uma classe
+"""
 
-    def calculaArea(self):#self é um argumento de metodos
-        self.area = 3.14*(self.raio**2)
-    def calculaVolume(self, altura):
-        self.volume = 3.4*(self.raio**2)*altura
-        
+class Retangulo:
+    ladoA = None
+    ladoB = None
+
+    def __init__(self, ladoA, ladoB):#instanciando
+        self.ladoA = ladoA
+        self.ladoB = ladoB
+
+"""
+Herança é um mecanismo cujo objetivo é facilitar o reaproveitamento de código
+"""
+
+class pessoa:#classe mãe
+    cidade = "padrão"
+    UF = "padrão"
+    bairro = "padrão"
+
+    def estado(self):
+        self.cidade = input("Qual a sua Cidade? ")
+        self.UF = input("Pertence a qual UF? ")
+        self.bairro = input("Você está em qual bairro? ")
+        self.nome = input("OK! E qual seu nome? ")
+        self.idade = input("qual sua idade? ")
+        self.corCabelo = input("Se você tem cabelo, qual a cor? ")
+
+
+class dadosPessoais(pessoa):
+    nome = "padrão"
+    idade = "padrão"
+    corCabelo = "padrão"
+
+user = dadosPessoais() #variavel recebe classe
+user.estado()
+print("OK! ", user.nome, "Você mora em ", user.cidade, ".", user.UF, "No bairro", user.bairro, "e tem ", user.idade, "anos.")
+
