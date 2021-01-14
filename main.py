1d3eb6bb794972b4d5f3fc77afb26ff32e35bb6a
icsalgado <icsalgado@icloud.com>
2021-01-14 03:39:49 -0300

    Estruturas de repetição

--- main.py
+++ main.py
@@ -1,42 +1,12 @@
-#Variáveis, operadores aritméticos/relacionais/lógicos, if/else
 #Estruturas de repetição
-
-### variáveis ###
-inteiros = 2021
-reais = 2.46
-booleanos = True
-frase = "curso Python"
-
-#saida de dados
-print(inteiros, reais, frase)#virgulas concatenam
-
-### Operadores aritméticos ###
-x = 10
-y  = 5
-soma = x+y
-sub = x-y
-div = x/y
-div2 = y/x
-mult = x*y
-exp = x**y
-print(soma, sub, div, div2, mult, exp)
-
-### Operadores relacionais/lógicos if/else ###
-var1 = 15
-var2 = 10
-if (var1 > var2):#dois pontos é como um imperativo faça!
-    print(var1, " > ", var2)
-    if(var1 + var2 >= 100):
-        print("maior que 100")
-    else:
-        if(var1 + var2 != 25):
-            print("diferente de 25")
-        else:
-            if var1 + var2 == 25:#parenteses não são obrigatórios
-                print("soma é igual a ", var1+var2)
-            else:
-                print("encadeados")
-if var1-var2 and var2-var1 > 0:
-    print("AND")
-elif var1-var2 or var2-var1 > 0:
-    print("OR")
+### WHILE ###
+num = 0
+while (num < 10):#parenteses não são obrigatórios
+    print("Numero atual ", num)
+    num = num + 1 #incremento para evitar loop infinito
+### FOR ###
+print("tabuada")
+for i in range(10):#conta 10 repetições
+    for j in range(10):
+        print(i, " x ", j, "|", i*j)
+    print("__________________")
