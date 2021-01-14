8cc7629e20e3b271f82407d146471865a250e99c
icsalgado <icsalgado@icloud.com>
2021-01-14 14:04:23 -0300

    Manipulação de arquivos externos

--- main.py
+++ main.py
@@ -1,40 +1,22 @@
-"""
-Método construtor é invocado quando a classe é instanciada, que é o nome dado
-à ação de criar objeros a partir de uma classe
-"""
+def criarArquivo():
+    arquivo = open("dados.txt","w")#w abre
+    arquivo.close()
+    print("txt criado")
 
-class Retangulo:
-    ladoA = None
-    ladoB = None
+def escreveArquivo():
+    nome = input("seu nome: ")
+    arquivo = open("dados.txt","a")#a altera, escreve
+    arquivo.write(nome)
+    arquivo.close()
 
-    def __init__(self, ladoA, ladoB):#instanciando
-        self.ladoA = ladoA
-        self.ladoB = ladoB
+def lerArquivo():
+    arquivo = open("dados.txt", "r")#r ler
+    line = arquivo.readline()
+    while line != "":
+        print(line)
+        line = arquivo.readline()
+    arquivo.close()
 
-"""
-Herança é um mecanismo cujo objetivo é facilitar o reaproveitamento de código
-"""
-
-class pessoa:#classe mãe
-    cidade = "padrão"
-    UF = "padrão"
-    bairro = "padrão"
-
-    def estado(self):
-        self.cidade = input("Qual a sua Cidade? ")
-        self.UF = input("Pertence a qual UF? ")
-        self.bairro = input("Você está em qual bairro? ")
-        self.nome = input("OK! E qual seu nome? ")
-        self.idade = input("qual sua idade? ")
-        self.corCabelo = input("Se você tem cabelo, qual a cor? ")
-
-
-class dadosPessoais(pessoa):
-    nome = "padrão"
-    idade = "padrão"
-    corCabelo = "padrão"
-
-user = dadosPessoais() #variavel recebe classe
-user.estado()
-print("OK! ", user.nome, "Você mora em ", user.cidade, ".", user.UF, "No bairro", user.bairro, "e tem ", user.idade, "anos.")
-
+criarArquivo()
+escreveArquivo()
+lerArquivo()
