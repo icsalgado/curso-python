0aad3c8f9996e7925d7d124eb9c582f4c06194e0
icsalgado <icsalgado@icloud.com>
2021-01-14 04:29:45 -0300

    Funções em python

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-#Métodos de strings len|lower|upper
+#Funções
+def aula ():#cria função
+    print('aula de funções')
 
-curso = "Curso de Programação em Python"
+def naula ():
+    print('aula x')
 
+aula()#chama função
+naula()
 
-print(len(curso))#conta os caracteres, inclusive espaços em branco
-print(curso.lower())#altera o caractere em caixa alta para caixa baixa
-print(curso.upper())#altera tudo para caixa alta
+def cliente (nome):#função com parametro
+    print('Ola, ',nome)
+
+cliente("Mundo")#chama função com argumento
+
+def entrada (user):
+    print("obrigado ", user)
+
+snome = input('Qual seu nome?')#entrada de dados
+
+entrada(snome)#chama função com argumento recolhido da entrada
+
+def soma (num1, num2):#função dinamica
+    resultado = num1 + num2
+    return resultado
+
+print(soma(5,6))
 
 
